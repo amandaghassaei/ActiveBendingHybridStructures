e92b78aeb6c696c682ff5530d06b656ebf295b61
amandaghassaei <amandaghassaei@gmail.com>
2016-11-03 18:50:35 -0400

    fixing edge class

--- js/edge.js
+++ js/edge.js
@@ -2,81 +2,56 @@
  * Created by ghassaei on 9/16/16.
  */
 
-var beamMaterialHighlight = new THREE.MeshLambertMaterial({color: 0xffffff});
-var beamMaterialDelete = new THREE.MeshBasicMaterial({color:0xff0000});
-var beamGeometry = new THREE.CylinderGeometry(0.1, 0.1, 1);
+var edgeMaterialHighlight = new THREE.MeshLambertMaterial({color: 0xffffff});
+var edgeMaterialDelete = new THREE.MeshBasicMaterial({color:0xff0000});
+var edgeMaterial = new THREE.MeshBasicMaterial({color:0xb67df0});
+var edgeGeometry = new THREE.CylinderGeometry(0.1, 0.1, 1);
 
-function Beam(nodes, globals){
+function Edge(nodes, parent){
 
-    this.type = "beam";
-    nodes[0].addBeam(this);
-    nodes[1].addBeam(this);
+    this.type = "edge";
+    nodes[0].addEdge(this);
+    nodes[1].addEdge(this);
     this.nodes = nodes;
 
-    this.material = new THREE.MeshLambertMaterial();
-    this.object3D = new THREE.Mesh(beamGeometry, this.material);
-    this.setDefaultColor();
-    this.object3D._myBeam = this;
-    globals.threeView.sceneAdd(this.object3D);
-    this.render();
+    this.object3D = new THREE.Mesh(edgeGeometry, edgeMaterial);
+    this.object3D._myEdge = this;
+    parent.add(this.object3D);
+    this.parent = parent;
+    this.update();
 }
 
-Beam.prototype.highlight = function(){
-    if (globals.deleteMode) this.object3D.material = beamMaterialDelete;
-    else this.object3D.material = beamMaterialHighlight;
+Edge.prototype.highlight = function(){
+    this.object3D.material = edgeMaterialHighlight;
 };
 
-Beam.prototype.unhighlight = function(){
-    if (this.material) this.object3D.material = this.material;
+Edge.prototype.unhighlight = function(){
+    this.object3D.material = edgeMaterial;
 };
 
-Beam.prototype.setDefaultColor = function(){
-    this.object3D.material.color.setHex(0xaaaaaa);
-};
-
-Beam.prototype.setColor = function(hex){
-    this.object3D.material.color.setHex(hex);
-};
-
-Beam.prototype.setHSLColor = function(val, max, min){
-    if (val === null){
-        this.object3D.material.color.setHex(0x000000);
-        return;
-    }
-    var scaledVal = (val - min)/(max - min) * 0.7;
-    var color = new THREE.Color();
-    color.setHSL(scaledVal, 1, 0.5);
-    this.object3D.material.color.set(color);
-};
-
-Beam.prototype.getLength = function(){
+Edge.prototype.getLength = function(){
     var vertex1Pos = this.nodes[0].getPosition();
     var vertex2Pos = this.nodes[1].getPosition();
     return vertex1Pos.sub(vertex2Pos).length();
 };
 
-Beam.prototype.getVector = function(fromNode){
+Edge.prototype.getVector = function(fromNode){
     var toNode = this.nodes[0];
     if (this.nodes[0] == fromNode) toNode = this.nodes[1];
     return toNode.getPosition().sub(fromNode.getPosition());
 };
 
-Beam.prototype.isFixed = function(){
+Edge.prototype.isFixed = function(){
     return this.nodes[0].fixed && this.nodes[1].fixed;
 };
 
-Beam.prototype.getForce = function(){
-    if (this.isFixed()) return null;
-    //todo
-};
-
-Beam.prototype.getOtherNode = function(node){
+Edge.prototype.getOtherNode = function(node){
     if (this.nodes[0] == node) return this.nodes[1];
     return this.nodes[0];
 };
 
-Beam.prototype.setDeleteMode = function(){
-    this.object3D.material = beamMaterialDelete;
+Edge.prototype.setDeleteMode = function(){
+    this.object3D.material = edgeMaterialDelete;
 };
 
 
@@ -84,19 +59,19 @@ Beam.prototype.setDeleteMode = function(){
 
 //render
 
-Beam.prototype.getObject3D = function(){
+Edge.prototype.getObject3D = function(){
     return this.object3D;
 };
 
-Beam.prototype.getNodes = function(){
+Edge.prototype.getNodes = function(){
     return this.nodes;
 };
 
-Beam.prototype.render = function(){
+Edge.prototype.update = function(){
     this.object3D.scale.y = this.getLength();
-    var beamAxis = this.nodes[0].getPosition().sub(this.nodes[1].getPosition());
-    var axis = (new THREE.Vector3(0,1,0)).cross(beamAxis).normalize();
-    var angle = Math.acos(new THREE.Vector3(0,1,0).dot(beamAxis.normalize()));
+    var edgeAxis = this.nodes[0].getPosition().sub(this.nodes[1].getPosition());
+    var axis = (new THREE.Vector3(0,1,0)).cross(edgeAxis).normalize();
+    var angle = Math.acos(new THREE.Vector3(0,1,0).dot(edgeAxis.normalize()));
     var quaternion = (new THREE.Quaternion()).setFromAxisAngle(axis, angle);
     var position = (this.nodes[0].getPosition().add(this.nodes[1].getPosition())).multiplyScalar(0.5);
     this.object3D.position.set(position.x, position.y, position.z);
@@ -107,14 +82,14 @@ Beam.prototype.render = function(){
 
 //deallocate
 
-Beam.prototype.destroy = function(){
+Edge.prototype.destroy = function(){
     var self = this;
     _.each(this.nodes, function(node){
-        node.removeBeam(self);
+        node.removeEdge(self);
     });
-    globals.threeView.sceneRemove(this.object3D);
-    this.object3D._myBeam = null;
+    this.parent.remove(this.object3D);
+    this.parent = null;
+    this.object3D._myEdge = null;
     this.object3D = null;
-    this.material = null;
     this.nodes = null;
 };
